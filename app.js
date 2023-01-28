b1fc716dc8ca69dcaf25738f2195b0f59fdc674d
Nitish jha <nitishjha6519@gmail.com>
2023-01-28 12:07:05 +0530

    Revert "removed index and added its content to app"
    This reverts commit edc2da6b3dbb247f290c15754ab482cc911a21b8.

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
-const PORT = process.env.PORT || 5000;
-const URL = `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPASS}@cluster0.eqvbavg.mongodb.net/RealEstate?retryWrites=true&w=majority`;
-mongoose.connect(
-  URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) {
-      console.log("err " + err);
-    } else {
-      console.log("connected to DB");
-    }
-  }
-);
 
 const jwt = require("jsonwebtoken");
 const bodyparser = require("body-parser");
@@ -56,4 +40,4 @@ app.use("/property", async (req, res, next) => {
 app.use("/property", propertyRoute);
 
 // in the routes we have user.js and login routes are definedthere
-app.listen(PORT, () => console.log("Server is up at 5000"));
+module.exports = app;
